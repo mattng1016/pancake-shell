4fa948edc2e8d1532b7aee4aabc072f20dfcf9b3
mattng1016 <mattng1016@gmail.com>
2026-09-03 17:46:34 -0700

    implemented built in cd and exit command

--- shell.c
+++ shell.c
@@ -5,7 +5,26 @@
 #include <string.h>
 #include <unistd.h>
 
-#define deliminator " /"
+#define deliminator " "
+
+static char* initPath = "/bin/";
+static char* usrPath = "/usr/bin/";
+const char* builtInFunction[] = {"pancake_exit", "[pancake_cd"};
+
+void pancake_exit() {
+  exit(0);
+}
+
+void pancake_cd(char** args) {
+  if (args[2] != NULL) {
+    printf("Too many arguments\n");
+    return;
+  }  
+  if (chdir(args[1]) == -1) {
+    printf("Path not found\n");
+  }
+  return;
+}
 
 // Parses given line and returns pointer of char pointers
 char **parseLine(char *line) {
@@ -49,6 +68,7 @@ char *readLine() {
   return buf;
 }
 
+// Execute given arguments
 void executeArg(char **args) {
   pid_t pID = fork();
   if (pID < 0) {
@@ -62,6 +82,38 @@ void executeArg(char **args) {
   }
 }
 
+// Searches if command executable exists
+void searchExecutable(char** args) {
+  char* temp = strdup(args[0]); 
+  int len = strlen(temp);
+  char* path1 = strdup(initPath);
+  char* path2 = strdup(usrPath);
+  strncat(path1, temp, len);
+  strncat(path2, temp, len);
+
+  if (access(path1, X_OK) == -1) {
+    if (access(path2, X_OK) == -1) {
+      printf("pancake: command not found: %s\n", temp);
+      return;
+    }
+  }
+  executeArg(args);
+}
+
+// Search if args contains built-in command
+void searchBuiltIn(char** args) {
+  if (strcmp(args[0], "exit") == 0) {
+    pancake_exit();
+    return;
+  }
+  if (strcmp(args[0], "cd") == 0) {
+    pancake_cd(args);
+    return; 
+  }
+
+  searchExecutable(args);
+}
+
 int main(int argc, char *argv[]) {
   char *line;
   char **args;
@@ -71,7 +123,7 @@ int main(int argc, char *argv[]) {
     printf("pancake> ");
     line = readLine();
     args = parseLine(line);
-    executeArg(args);
+    searchBuiltIn(args);
 
   } while (temp);
 
